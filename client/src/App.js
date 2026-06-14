1e528e0690fc186f0523589b35a7ac356bd2b5f0
Koa Lee <koalee@berkeley.edu>
2026-06-13 17:02:48 -0700

    Styling experimentation

--- client/src/App.js
+++ client/src/App.js
@@ -19,15 +19,15 @@ function App() {
         <sub>Creative</sub>
       </div>
 
-      <div className="info-card">
+      {/* <div className="info-card">
         <h2>Overview</h2>
-
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vitae quam fermentum, consequat nisl ac, volutpat nulla. Praesent vitae leo nec risus cursus tristique. Vestibulum rutrum feugiat dolor sit amet vulputate. Nam eu risus porta, auctor lorem sed, tempus orci. Vivamus dapibus fringilla nulla vel vulputate. Praesent quis gravida odio, eu iaculis turpis. Maecenas vitae pretium dolor, nec gravida risus. Praesent pellentesque sed turpis ut accumsan.
       </div>
 
       <div className="info-card">
         <h2>Overview</h2>
-
-      </div>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vitae quam fermentum, consequat nisl ac, volutpat nulla. Praesent vitae leo nec risus cursus tristique. Vestibulum rutrum feugiat dolor sit amet vulputate. Nam eu risus porta, auctor lorem sed, tempus orci. Vivamus dapibus fringilla nulla vel vulputate. Praesent quis gravida odio, eu iaculis turpis. Maecenas vitae pretium dolor, nec gravida risus. Praesent pellentesque sed turpis ut accumsan.
+      </div> */}
 
     </div>
   );
