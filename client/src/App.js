921c8783d5658458eeb5d58756fb25d427d4afc0
Koa Lee <koalee@berkeley.edu>
2026-06-15 13:55:50 -0700

    commenting out test code for first website upload

--- client/src/App.js
+++ client/src/App.js
@@ -20,13 +20,13 @@ function App() {
       </div>
 
       {/* <div className="info-card">
-        <h2>Overview</h2>
+        <h2>Projects</h2>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vitae quam fermentum, consequat nisl ac, volutpat nulla. Praesent vitae leo nec risus cursus tristique. Vestibulum rutrum feugiat dolor sit amet vulputate. Nam eu risus porta, auctor lorem sed, tempus orci. Vivamus dapibus fringilla nulla vel vulputate. Praesent quis gravida odio, eu iaculis turpis. Maecenas vitae pretium dolor, nec gravida risus. Praesent pellentesque sed turpis ut accumsan.
       </div>
 
       <div className="info-card">
-        <h2>Overview</h2>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vitae quam fermentum, consequat nisl ac, volutpat nulla. Praesent vitae leo nec risus cursus tristique. Vestibulum rutrum feugiat dolor sit amet vulputate. Nam eu risus porta, auctor lorem sed, tempus orci. Vivamus dapibus fringilla nulla vel vulputate. Praesent quis gravida odio, eu iaculis turpis. Maecenas vitae pretium dolor, nec gravida risus. Praesent pellentesque sed turpis ut accumsan.
+        <h2>Contact</h2>
+        <li></li>
       </div> */}
 
     </div>
