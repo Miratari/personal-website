17551a29653a7586ed2bc3fad3e6640c55207c8e
Koa Lee <koalee10@gmail.com>
2026-07-20 14:20:40 -0700

    updated design language for a more modern rounded look

--- client/src/App.js
+++ client/src/App.js
@@ -22,7 +22,7 @@ function Project({ title, type, demo, note, src, desc, yt, ig }) {
     <div className="project">
       <div className="title" style={{gridArea: "title"}}>{title}</div>
       <div className="type" style={{gridArea: "type"}}>{type}</div>
-      <a className="links" style={{gridArea: "demo"}} href={demo}>{demo ? "Site" : null}</a>
+      <a className="links" style={{gridArea: "demo"}} href={demo}>{demo ? "Website" : null}</a>
       <a className="links" style={{gridArea: "source"}} href={src}>{src ? "GitHub" : null}</a>
       <a className="links" style={{gridArea: "yt"}} href={yt}>{yt ? "YouTube" : null}</a>
       <a className="links" style={{gridArea: "ig"}} href={ig}>{ig ? "Instagram" : null}</a>
@@ -46,16 +46,18 @@ function App() {
 
         <section id="about" style={{gridArea: "about"}}>
           <header>
-            About
+            Info
           </header>
           <div class="section-content">
-            Hi, I'm <b>Koa!</b> I like to make stuff.<br/>
-            <br/>
-            I'm a UC Berkeley Computer Science graduate, and enjoy making useful apps and cool interfaces. On this site you can find a handful of my open-source creations and contributions. When I'm not on the computer, I like to play and write music.<br/>
-            <br/>
-            To learn more, or to get in touch, keep scrolling!<br/>
-            <br/>
-            <br/>
+            <p>
+              <h1>Hi, I'm <b>Koa!</b> I like to make stuff.</h1><br/>
+              <br/>
+              I'm a UC Berkeley CS graduate who enjoys making useful apps and cool interfaces. On this site you can find a handful of my open-source creations and contributions. When I'm not on the computer, I also like to play and write music.<br/>
+              <br/>
+              To learn more, or to get in touch, keep scrolling!<br/>
+              <br/>
+              <br/>
+            </p>
           </div>
         </section>
 
@@ -69,39 +71,39 @@ function App() {
               type="Web App - 2023"
               demo="https://typed-mauve.vercel.app/"
               src="https://github.com/noah-whelann/typing-game"
-              desc="A full-stack typing game using React and MongoDB. Generates random words and calculates user typing speed and accuracy, storing user data between matches."
+              desc="A full-stack typing game using React and MongoDB."
               note="Word API broken as of Jul 2026"/>
             <Project
               title="Simple Discord Status"
               type="Utility - 2025"
               src="https://github.com/Miratari/steam-deck-simple-discord-status"
-              desc="A Steam Deck plugin to display the current active game via Discord status. Integrates directly with Discord API."/>
+              desc="A Steam Deck plugin to display the current active game via Discord status."/>
             <Project
               title="Home Server"
               type="IT/Networking - 2026"
-              desc="My personal Docker-based Linux home media server. Created and maintained with Claude Code, with custom domain routing powered by Nginx Proxy Manager."/>
+              desc="Personal Docker-based Linux home media server."/>
             <Project
               title="Playlist Name Generator"
               type="Utility - 2022"
               src="https://github.com/ehuang1126/Playlist-Name-Generator"
-              desc="Script to generate content-aware Spotify playlist titles from AI analysis of song lyrics. Utilizes Cohere NLP, made for CalHacks 9.0 hackathon."/>
+              desc="Generates content-aware Spotify playlist titles from AI analysis of song lyrics. Made for CalHacks 9.0 hackathon."/>
           </div>
         </section>
         
         <section style={{gridArea: "work"}}>
           <header>
-            Work Experience
+            Experience
           </header>
           <div class="section-content">
             <Project
               title="SDE Intern, AWS"
               type="Amazon Timestream - 2024"
               demo="https://aws.amazon.com/timestream/"
-              desc="I designed and implemented scalable infrastructure upgrades for over 100,000 InfluxDB instances. Enabled dynamic backend integration with AWS services such as S3, EC2, ECS (Kubernetes), CloudWatch, and IAM."/>
+              desc="Designed and implemented scalable infrastructure upgrades for Amazon Timestream for InfluxDB."/>
             <Project
               title="Web Developer"
               type="Angeleno Homes LLC - 2023"
-              desc="Freelance consultation and website development for LA-based real estate company. I designed a simple web interface for collecting and processing online home valuation requests."/>
+              desc="Freelance consultation and website development for LA-based real estate company."/>
           </div>
         </section>
 
@@ -115,7 +117,7 @@ function App() {
               type="Music"
               yt="https://www.youtube.com/@tioatberkeley"
               ig="https://www.instagram.com/tioatberkeley/"
-              desc="UC Berkeley's anime, video game, and film music orchestra. I've arranged and conducted pieces for full orchestra and small jazz ensembles, and have performed as a percussionist, drummer, and bassist."
+              desc="UC Berkeley's anime, video game, and film music orchestra. Arranger, conductor, percussionist, drummer, and bassist."
               note="Carousel of YouTube videos coming soon"/>
             {/* <Project
               title="Photography"
@@ -125,7 +127,7 @@ function App() {
           </div>
         </section>
 
-        <div class="contact-container" style={{gridArea: "contact"}}>
+        <section class="contact-container" style={{gridArea: "contact"}}>
           <Contact 
             icon={github} 
             text="Github" 
@@ -145,7 +147,7 @@ function App() {
             icon={mail} 
             text="Email" 
             subtext="koalee10@gmail.com"/>
-        </div>
+        </section>
 
         <footer style={{gridArea: "footer"}}>
           Updated July 2026.<br/>Designed and coded by hand, not AI.
