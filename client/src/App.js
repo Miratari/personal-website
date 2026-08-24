7418a43d1f72d6548d190926755f9e6b5c169e1a
Koa Lee <koalee10@gmail.com>
2026-08-24 00:49:11 -0700

    updated formatting bugs

--- client/src/App.js
+++ client/src/App.js
@@ -29,8 +29,9 @@ function App() {
         <h2 id="t2" style={{gridArea: "t2"}}>Software</h2>
         <h1 id="t3" style={{gridArea: "t3"}}>Engineer</h1>
         <h2 id="t4" style={{gridArea: "t4"}}>Web</h2>
-        <h1 id="t5" style={{gridArea: "t5"}}>Developer</h1>
-        <h1 id="t6" style={{gridArea: "t6"}}>Creative</h1>
+        <h1 id="t5" style={{gridArea: "t5"}}>Designer</h1>
+        <h2 id="t6" style={{gridArea: "t6"}}>&</h2>
+        <h1 id="t7" style={{gridArea: "t7"}}>Creative</h1>
 
         <p id="about" style={{gridArea: "about"}}>Hi, I'm Koa! I like to make stuff. This is my personal website, where you can find links to open-source repositories and other creative projects.</p>
 
@@ -39,7 +40,7 @@ function App() {
         <NavLink id="l2" style={{gridArea: "l2"}} to="/music">Music</NavLink>
         <NavLink id="l3" style={{gridArea: "l3"}} to="/photos">Photos</NavLink>
         {/* <h3 id="c-title" style={{gridArea: "c-title"}}>Contact</h3> */}
-        <a id="l4" style={{gridArea: "l4"}} href="https://github.com/Miratari">Github</a>
+        <a id="l4" style={{gridArea: "l4"}} href="https://github.com/Miratari">GitHub</a>
         <a id="l5" style={{gridArea: "l5"}} href="https://www.linkedin.com/in/koa-lee-476b2b226/">LinkedIn</a>
         <a id="l6" style={{gridArea: "l6"}} href="mailto:koalee10@gmail.com">Email</a>
 
--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from "react-router";
 import './index.css';
-import App from './App';
-import Code from './Code';
+import App from './App.js';
+import Code from './Code.js';
 import reportWebVitals from './reportWebVitals';
 
 const root = document.getElementById("root");
